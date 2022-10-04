b422971924b0f20f44e96f14b1a1f3efad794721
ghuitson <115020128+ghuitson@users.noreply.github.com>
2022-10-04 16:19:58 +0100

    Update streamline_app.py
    added emoji

--- streamline_app.py
+++ streamline_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🍲Omega 3 & Blueberry Oatmeal')
+streamlit.text('🧋Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🥪 Avacado on Toast')
