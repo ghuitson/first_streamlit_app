b2573ec9cc82a56848160f509c78b7a74966f4bf
ghuitson <115020128+ghuitson@users.noreply.github.com>
2022-10-04 16:21:33 +0100

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -7,3 +7,5 @@ streamlit.text('🍲Omega 3 & Blueberry Oatmeal')
 streamlit.text('🧋Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🥪 Avacado on Toast')
+
+streamlit.header('🍓🍌 Build Your Own Breakfast Smoothie! 🍉🍇')
