fdb32bc380abcd28f3bdcdf703d8ba4d8b382005
ghuitson <115020128+ghuitson@users.noreply.github.com>
2022-10-07 16:23:23 +0100

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -43,6 +43,13 @@ try:
 except URLError as e:
   streamlit.error()
   
+add_my_fruit = streamlit.text_input('What fruit would you like to add:')
+
+if streamlit.button('Add a fruit to list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT fruit_name from fruit_load_list")
 my_data_row = my_cur.fetchall()
@@ -57,9 +64,4 @@ def insert_row_snowflake(new_fruit):
     
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add:')
 
-if streamlit.button('Add a fruit to list'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_function = insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_function)
