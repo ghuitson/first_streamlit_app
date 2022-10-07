2cb8e9b62bd1f3b8028b2210fef4cef1d4375827
ghuitson <115020128+ghuitson@users.noreply.github.com>
2022-10-07 12:30:30 +0100

    Update streamline_app.py
    insert fruits to snowflake

--- streamline_app.py
+++ streamline_app.py
@@ -44,9 +44,12 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT fruit_name from fruit_load_list")
+
 my_data_row = my_cur.fetchall()
 streamlit.header("The Fruit Load List contains:")
 streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add:')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list(fruit_name) values ('from streamlit')");
