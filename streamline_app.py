df221f4919531644796e1ce28927f134de32c901
ghuitson <115020128+ghuitson@users.noreply.github.com>
2022-10-07 12:26:47 +0100

    Update streamline_app.py
    fruit input

--- streamline_app.py
+++ streamline_app.py
@@ -47,3 +47,6 @@ my_cur.execute("SELECT fruit_name from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The Fruit Load List contains:")
 streamlit.dataframe(my_data_row)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add:')
+streamlit.write('The user entered ', add_my_fruit)
